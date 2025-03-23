62575a11102b86f41aaef20946ba2f64d4228fd8
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-03-23 19:28:31 +0900

    문제 풀이

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,9 @@
+package construct;
+
+public class MemberDefault {
+    String name;
+
+    MemberDefault() {
+        System.out.printf("생성자 호출");
+    }
+}
---
+++ src/pack/b/User.java
@@ -0,0 +1,8 @@
+package pack.b;
+
+public class User {
+
+    public User(){
+        System.out.println("ㅠㅐ키지 pack.b 회원 생성");
+    }
+}
